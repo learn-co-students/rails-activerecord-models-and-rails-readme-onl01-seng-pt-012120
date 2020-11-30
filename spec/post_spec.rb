0ad2763bd6473f98c840ec08b003c6b82dd371f4
Your Name <you@example.com>
2020-11-29 20:15:40 -0500

    Done.

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -7,3 +7,11 @@ describe Post do
     end
  
 end
+
+describe Post do
+    it 'has a summary' do
+        post = Post.create!(title: "My title", description: "The post description")
+        expect(post.post_summary).to eq("My title - The post description")
+    end
+
+end 
